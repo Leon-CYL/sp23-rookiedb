332bd743651bdef086a23b011749007a65dfca48
Leon123-Lin <65735858+Leon123-Lin@users.noreply.github.com>
2023-07-10 16:59:14 -0700

    Update Table.java

--- src/main/java/edu/berkeley/cs186/database/table/Table.java
+++ src/main/java/edu/berkeley/cs186/database/table/Table.java
@@ -406,7 +406,7 @@ public class Table implements BacktrackingIterable<Record> {
      */
     public BacktrackingIterator<RecordId> ridIterator() {
         // TODO(proj4_part2): Update the following line
-        LockUtil.ensureSufficientLockHeld(tableContext, LockType.S);
+        LockUtil.ensureSufficientLockHeld(tableContext, LockType.NL);
 
         BacktrackingIterator<Page> iter = pageDirectory.iterator();
         return new ConcatBacktrackingIterator<>(new PageIterator(iter, false));
@@ -420,7 +420,7 @@ public class Table implements BacktrackingIterable<Record> {
      */
     public BacktrackingIterator<Record> recordIterator(Iterator<RecordId> rids) {
         // TODO(proj4_part2): Update the following line
-        LockUtil.ensureSufficientLockHeld(tableContext, LockType.S);
+        LockUtil.ensureSufficientLockHeld(tableContext, LockType.NL);
         return new RecordIterator(rids);
     }
 
